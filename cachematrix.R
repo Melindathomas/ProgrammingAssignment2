87def05c32d5d4b199b412c69485c5a29f609b9d
Melindathomas <Melindathomas@users.noreply.github.com>
2014-05-25 18:59:06 +1200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix(k,l)) {
+
+
+<- functon (y = (x^2))## assigning squared matrix value for y
+
+
+if(x=k,l) { ##if the matrix is same and an an unsquared on, proceed with inverse
+
+> cacheSolve <- function (z<- x= (k,l))
+
+> return(z)
+
 
 }
 
+else {
+>- function >- a ="inverse" [x] ## doing the inverse value if the matrix is not being inversed by the cachesolve code
 
-## Write a short comment describing this function
+>return (a)  ## returning the value for "a" if the matrix is not being inversed
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
+
+else if {
+
+  >  function b >- solve (y)) { ## if the matrix is a squared one incorparate a different value-y
+
+  return (b) ## returing the inversed square matrix
+  
+  split (!= z, a, b) ## spilt the program if the values is the values are not z, a and b
+  
+  }
+ 
+}
+
+
